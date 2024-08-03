2cecb761409abc8a0a2fcc6f26d988f335cc1bb5
nhut321 <znhut12z@gmail.com>
2024-08-04 00:02:02 +0700

    update index size

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ require("./initDB")();
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '20mb'}));
-app.use(express.urlencoded({ limit: '20mb',  extended: true }));
+app.use(express.json({ limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb',  extended: true }));
 
 // Routes
 const musicRouter = require("./Router/music");
