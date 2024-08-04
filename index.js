ac2bffe1e7e3a9b2b6e07b39b9ea4e2994dd467b
nhut321 <znhut12z@gmail.com>
2024-08-04 20:39:02 +0700

    update bo sung

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ const io = socketIo(server, {
   });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["https://gianglethuylinh.site", "http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+  }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
